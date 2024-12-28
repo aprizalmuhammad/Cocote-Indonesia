390c2ef25b42988d7212ebd426a286e16aa98909
aprizal <aprizal17a@gmail.com>
2024-12-28 17:44:03 +0700

    menambahkan modal box pada menu produk

--- js/script.js
+++ js/script.js
@@ -1,15 +1,67 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
 
-//  klik diluar sidebar untuk menghilangkan nav sidebar
-const hamburger = document.querySelector("#hamburger-menu");
+//toggle kelas active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener("click", function(e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
-    }
+document.querySelector("#search-button").onclick = (event) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  event.preventDefault();
+};
+
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (event) => {
+  shoppingCart.classList.toggle("active");
+  shoppingCart.focus();
+  event.preventDefault();
+};
+
+//  klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (event) => {
+    itemDetailModal.style.display = "flex";
+    event.preventDefault();
+  };
+});
+
+// Klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik tombol diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
